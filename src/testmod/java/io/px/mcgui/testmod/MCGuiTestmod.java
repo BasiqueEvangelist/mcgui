bbef6c44e8da7db8d0e90eb8a567fe6d167bb7e1
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-02-25 14:47:18 +0300

    Add command to open screen ingame

--- src/testmod/java/io/px/mcgui/testmod/MCGuiTestmod.java
+++ src/testmod/java/io/px/mcgui/testmod/MCGuiTestmod.java
@@ -4,11 +4,20 @@
  */
 package io.px.mcgui.testmod;
 
+import io.px.mcgui.MCGUI;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.util.Identifier;
 
 public class MCGuiTestmod implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        CommandRegistrationCallback.EVENT.register((re, d) -> {
+            re.register(CommandManager.literal("opentestgui").executes(ctx -> {
+                MCGUI.openScreen(new Identifier("mcgui-testmod", "test"),  null);
+                return 0;
+            }));
+        });
     }
 }
